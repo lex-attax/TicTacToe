1ecd2638f14b6dd53c1d0dc205703e549c31544d
lex-attax <lex.pavlovic@gmail.com>
2017-04-26 17:57:42 +0200

    Added method wait1Sec

--- AI.java
+++ AI.java
@@ -2,20 +2,30 @@ package tictactoeapplication;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 class AI {
 
 	public int pickSpot(TicTacToe game) {
 		ArrayList<Integer> choices = new ArrayList();
-			for (int i = 0; i < 9; i++) {
-				// If the slot is not taken, add it as a choice
-				if (game.board[i] == '-') {
-					choices.add(i + 1);
-				}
+		for (int i = 0; i < 9; i++) {
+			// If the slot is not taken, add it as a choice
+			if (game.board[i] == '-') {
+				choices.add(i + 1);
 			}
-			Random rand = new Random();
-			int choice = choices.get(Math.abs(rand.nextInt() % choices.size()));
-			return choice;
+		}
+		Random rand = new Random();
+		int choice = choices.get(Math.abs(rand.nextInt() % choices.size()));
+		return choice;
 	}
-	
+
+	public void wait1Sec() {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
